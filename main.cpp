1c8623e2ce35fdd2de054e35e63a79652ce3e269
ehank <emirhankotan140@gmail.com>
2025-04-17 13:56:03 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+/*
+    Lecture-Scheduler - A program made for showing info about lectures.
+    Copyright (C) 2025 Emirhan Kotan
+    Original work: https://github.com/Authlantech/Lecture-Scheduler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License version 3 for more details.
+
+    You should have received a copy of the GNU General Public License version 3
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -18,8 +36,17 @@ std::vector<Lecture> lectures;
 
 int main()
 {
+
+    char copyright_notice[400] =
+        "Lecture-Scheduler Copyright (C) 2025  Emirhan Kotan\n"
+        "Original work's source code: https://github.com/Authlantech/Lecture-Scheduler\n"
+        "This program comes with ABSOLUTELY NO WARRANTY;\n"
+        "This is free software, and you are welcome to redistribute it under certain conditions\n"
+        "For more details see the GNU General Public License version 3\n";
+
+    std::cout << copyright_notice << std::endl;
+
     std::string file_name;
-    std::cout << "Lecture Scheduler - Copyright (c) 2025 Emirhan Kotan\n";
 
     // Extract the text content in pdf file to a txt file :
 

e7e6855f415e12176d21151e8d5922e01544bfc4
ehank <emirhankotan140@gmail.com>
2025-04-21 22:45:43 +0300

    some changes

--- main.cpp
+++ main.cpp
@@ -23,7 +23,12 @@
 #include <sstream>
 #include <limits>
 #include <regex>
+#include <algorithm>
+#include <map>
+#include <utility>
 
+
+// Program user-defined datatypes 
 enum weekdays
 {
     monday,
@@ -32,7 +37,6 @@ enum weekdays
     thursday,
     friday
 };
-
 class LectureTime
 {
 protected :
@@ -67,8 +71,7 @@ public:
         this->_minute = other._minute;
     }
 };
-
-struct Lecture
+struct lecture
 {
     std::string code;
     int section;
@@ -78,108 +81,116 @@ struct Lecture
     std::string classroom;
 };
 
-std::vector<Lecture> lectures;
+typedef std::vector<struct lecture> section_pool; 
+typedef std::vector<std::vector<struct lecture>> course_pool;
+
+//Program functions
+std::vector<lecture> term_lectures;
+const std::vector<lecture> find_lectures(std::vector<std::string>lecture_data)
+{
+    std::vector<lecture>desired_lectures;
+    for(std::string& param : lecture_data)
+    {
+        std::vector<lecture> lecture_group;
+        std::size_t pos = param.find(":");
+        //Parameter consists course code and section :
+        if(pos != std::string::npos)
+        {
+            try
+            {
+                // Retrieve the course code of lecture :
+                std::string lecture_code = param.substr(0,pos);
+                // Retrieve the section of the lecture :
+                int section = std::stoi(param.substr(pos + 1));
+
+                // Search the lectures with given course code and section :
+                for(lecture l : term_lectures)
+                {
+                    if(l.code == lecture_code && l.section == section)
+                    {
+                        lecture_group.push_back(l);
+                    }
+                }
+            }
+
+            catch(std::invalid_argument const& ex)
+            {
+                std::cerr << std::endl << param << " : ";
+                std::cerr<< ex.what() << std::endl;
+            }
+
+            catch(std::out_of_range const& ex)
+            {
+                std::cerr << std::endl << param << " : ";
+                std::cerr << ex.what() << std::endl;
+            }
+        }
+
+        // Parameter consists only course code :
+        else
+        {
+            try
+            {
+                // Retrieve the course code of lecture :
+                std::string lecture_code = param.substr(0,pos);
+
+                // Search the lectures with given course code and section :
+                for(lecture l : term_lectures)
+                {
+                    if(l.code == lecture_code)
+                    {
+                        lecture_group.push_back(l);
+                    }
+                }
+            }
+
+            catch(std::out_of_range const& ex)
+            {
+                std::cerr << std::endl << param << " : ";
+                std::cerr << ex.what() << std::endl;
+            }
+
+        }
+
+        if(lecture_group.size() > 0)
+        {
+            std::sort(lecture_group.begin(), lecture_group.end(),[](const lecture& l1,const lecture& l2) {return l1.section < l2.section;});
+            for(auto l : lecture_group) desired_lectures.push_back(l);
+        }
+
+    }
+
+    return desired_lectures;
+}
 
 int main()
 {
-    char copyright_notice[400] =
+    char copyright_notice[500] =
         "Lecture-Scheduler Copyright (C) 2025 Emirhan Kotan\n"
         "Original source code: https://github.com/Authlantech/Lecture-Scheduler\n"
         "This program comes with ABSOLUTELY NO WARRANTY.\n"
         "This is free software, and you are welcome to redistribute it\n"
-        "under certain conditions which are stated in GNU General Public License version 3\nVisit: <https://www.gnu.org/licenses/gpl-3.0.html> for all details.\n";
+        "under certain conditions which are stated in GNU General Public License version 3"
+        "\nVisit: <https://www.gnu.org/licenses/gpl-3.0.html> for all details.\n";
 
     std::cout << copyright_notice << std::endl;
-
-    std::string file_name;
-
-    // Extract the text content in pdf file to a txt file :
-
-    std::ifstream file;
-    do
-    {
-        std::cout << "\n> Enter file path: " << std::flush;
-        std::cin >> file_name;
-        file.open(file_name);
-        if(file.is_open() == false) std::cout << "\n> File could not be opened!" << std::flush;
-    }while(file.is_open() == false);
-
-    std::string command("pdftotext -raw " + file_name);
-    system(command.c_str());
-    file.close();
-
-    file_name = file_name.substr(0,file_name.find_last_of(".")) + std::string(".txt");
-    file.open(file_name);
-
-    // Read txt file and extract lecture info :
-
-    std::string line;
-    char buffer[500];
-
-    std::vector<std::string> words;
-    std::string word;
-
-    do
-    {
-        line.clear();
-        memset(buffer,0,sizeof(buffer));
-        words.clear();
-        word.clear();
-
-        file.getline(buffer,500);
-        line = buffer;
-
-        if(line.find("DERS PROGRAM") != std::string::npos || line.find("DERS KODU") != std::string::npos ) continue;
-
-        std::istringstream iss(line);
-        while(iss >> word)
-        {
-            words.push_back(word);
-        }
-
-        if(words.size() <= 4) continue;
-
-        Lecture lecture;
-        lecture.code = words[2] + words[3];
-        lecture.section = std::stoi(words[1]);
-        lecture.weekday = words[4];
-        lecture.begining = LectureTime(std::stoi(words[5].substr(0,2 )), std::stoi(words[5].substr(3,2)));
-        lecture.ending = LectureTime(std::stoi(words[6].substr(0,2 )) ,std::stoi(words[6].substr(3,2)));
-        lecture.classroom = words[7];
-        for(int a = 8;a < words.size();a++)
-        {
-            lecture.classroom += '_' + words[a];
-        }
-        lectures.push_back(lecture);
-
-    }while(!file.eof());
-    file.close();
-
-    std::cout << "\n> File succesfully read!" << std::flush;
 
     /*
      *  COMMANDS :
      *
-     *  (list lectures)
-     *  > ll a                                      (list all of the lectures on a week)
-     *  > ll  <code> ...  .                        (list all the lecture with given code)
-     *  > ll  <code:section>... .                  (list all the lectures with given code and section)
-     *
-     *  (create program)
-     *  > cp <code>:<section(optional)>... .        (create all possible weekly programs with the codes and (optionally) sections provided
-     *                                              if you wish a specific section of a lecture you should add ':' after the lecture
-     *                                              code and type de section you wish to put in your weekly program)
-     *
-     *                                              example usage : > cp MATH1100:5 MATH1132 .
-     *                                              (this will generate all possible weekly programs with 5th section of MATH1100 and
-     *                                              all sections of MATH1132)
-     *
-     * >
+	 *  > exit                                      (exit the program)
+     *  > rd <filename>                             (read the lecture database from pdf file)    
+     * 
+     * 
+     *  > ll a                                      (list all the lectures on a week)
+     *  > ll  <course code> ...  .                  (list all the lecture with given course code)
+     *  > ll  <course code:section>... .            (list all the lectures with given course code and section)
+     *  
+     *  > cp  <code:section(optional)>              (list all possible weekly programs which can be created with given lectures)
      */
 
     // Go into a loop and ask for user directives :
 
-    bool exit = false;
     std::string input;
 
     do
@@ -194,21 +205,19 @@ int main()
             //Display all lectures :
             if(input == "a")
             {
-                std::cout << "\nALL LECTURES\n------" << std::endl;
-                for(auto& lecture : lectures)
+                std::cout << "\n\nALL LECTURES\n------" << std::endl;
+                for(auto& lecture : term_lectures)
                 {
                     std::cout << lecture.code << " " << lecture.section << " " << lecture.classroom << " " << lecture.weekday
                             << " " << lecture.begining.hour() << ":" << lecture.begining.minute() << " - " << lecture.ending.hour()
                             <<":" << lecture.ending.minute() << std::endl;
                 }
-                std::cout << "\n------\nALL LECTURES";
+                std::cout << "\n------\nALL LECTURES\n\n";
             }
 
             // Display lectures with given parameters :
            else
            {
-               std::vector<Lecture>desired_lectures;
-
                // Retrieve the parameters from user :
 
                std::vector<std::string> params;
@@ -219,84 +228,120 @@ int main()
                    params.push_back(input);
                }while(input != ".");
 
-               //Determine which lectures are asked to display :
+               // Retrieve the lectures from database :
+               std::vector<lecture> lectures = find_lectures(params);
 
-               for(std::string& param : params)
+               //Display the lecture info :
+
+               std::cout << "\n\n";
+               for(auto& lecture : lectures)
                {
-                   std::size_t pos = param.find(":");
-                   //Parameter consists lecture code and section :
-                   if(pos != std::string::npos)
-                   {
-                       try
-                       {
-                           // Retrieve the code of lecture :
-                           std::string lecture_code = param.substr(0,pos);
-                           //Retrieve the section of the lecture :
-                           int section = std::stoi(param.substr(pos + 1));
-
-                           // Search the lectures with given code and section :
-                           for(Lecture l : lectures)
-                           {
-                               if(l.code == lecture_code && l.section == section)
-                               {
-                                   desired_lectures.push_back(l);
-                               }
-                           }
-                       }
-
-                       catch(std::invalid_argument const& ex)
-                       {
-                            std::cerr << std::endl << ex.what() << std::endl;
-                       }
-
-                       catch(std::out_of_range const& ex)
-                       {
-                          std::cerr << std::endl << ex.what() << std::endl;
-                       }
-                   }
-
-                   // Parameter consists only lecture code :
-                   else
-                   {
-                       try
-                       {
-                           // Retrieve the code of lecture :
-                           std::string lecture_code = param.substr(0,pos);
-
-                           // Search the lectures with given code and section :
-                           for(Lecture l : lectures)
-                           {
-                               if(l.code == lecture_code)
-                               {
-                                   desired_lectures.push_back(l);
-                               }
-                           }
-                       }
-
-                       catch(std::out_of_range const& ex)
-                       {
-                           std::cerr << std::endl << ex.what() << std::flush;
-                       }
-
-                   }
-
+                   std::cout << lecture.code << " " << lecture.section << " " << lecture.classroom << " " << lecture.weekday
+                   << " " << lecture.begining.hour() << ":" << lecture.begining.minute() << " - " << lecture.ending.hour()
+                   <<":" << lecture.ending.minute() << std::endl;
                }
-
-               //Display the specified lectures :
-
-               for(Lecture&dl : desired_lectures)
-               {
-                   std::cout << std::endl << dl.code << " " << dl.section << " " << dl.classroom << " " <<
-                       dl.weekday << " " << dl.begining.hour() << ":" << dl.begining.minute() <<
-                           " " << dl.ending.hour() << ":" << dl.ending.minute() << std::endl;
-               }
-               desired_lectures.clear();
+               std::cout << "\n\n";
            }
         }
 
+        else if (input == "exit")
+        {
+            return EXIT_SUCCESS;
+		}
+
+        else if (input == "cp")
+        {
+
+        }
+
+        else if (input == "rd")
+        {
+            // Extract the lecture database from pdf file :
+
+            term_lectures.clear();
+            std::string file_name;
+            std::ifstream file;
+            do
+            {
+                std::cin >> file_name;
+                file.open(file_name);
+                if (file.is_open() == false) std::cout << "\n\n> File could not be opened!\n\n> Re-enter file name: " << std::flush;
+            } while (file.is_open() == false);
+
+            std::string command("pdftotext -raw " + file_name);
+            system(command.c_str());
+            file.close();
+
+            file_name = file_name.substr(0, file_name.find_last_of(".")) + std::string(".txt");
+            file.open(file_name);
+
+            // Read txt file and extract lecture info :
+
+            std::string line;
+            char buffer[500];
+
+            std::vector<std::string> words;
+            std::string word;
+
+            do
+            {
+                line.clear();
+                memset(buffer, 0, sizeof(buffer));
+                words.clear();
+                word.clear();
+
+                file.getline(buffer, 500);
+                line = buffer;
+
+                if (line.find("DERS PROGRAM") != std::string::npos || line.find("DERS KODU") != std::string::npos) continue;
+
+                std::istringstream iss(line);
+                while (iss >> word)
+                {
+                    words.push_back(word);
+                }
+
+                if (words.size() <= 4) continue;
+
+                lecture lecture;
+                lecture.code = words[2] + words[3];
+                lecture.section = std::stoi(words[1]);
+                lecture.weekday = words[4];
+                lecture.begining = LectureTime(std::stoi(words[5].substr(0, 2)), std::stoi(words[5].substr(3, 2)));
+                lecture.ending = LectureTime(std::stoi(words[6].substr(0, 2)), std::stoi(words[6].substr(3, 2)));
+                lecture.classroom = words[7];
+                for (int a = 8; a < words.size(); a++)
+                {
+                    lecture.classroom += '_' + words[a];
+                }
+                term_lectures.push_back(lecture);
+
+            } while (!file.eof());
+            file.close();
+
+            std::cout << "\n\n> File succesfully read!\n\n" << std::flush;
+        }
+
+		else if (input == "help")
+		{
+			std::cout << "\n\n> Available commands :\n"
+				<< "  > ll a                                        List all the lectures\n"			
+				<< "  > ll <course code:section(optional)> ...  <.>        List the lectures with given course code and (optionally) section\n"
+				<< "  > cp <course code:section(optional)> ...  <.>        Create all possible weekly programs with given lectures\n"
+				<< "  > exit                                        exit the app\n"
+				<< "  > rd <filename>                               read the lecture database from pdf file\n"
+				<< "  > clear                                       clear the console\n\n" << std::flush;
+		}
+
+        else if (input == "clear")
+        {
+            system("cls");
+            std::cout << copyright_notice << "\n\n";
+        }                      
+
         else
         {
-            std::cout << "\n> Invalid command!\n";
+            std::cout << "\n\n> Invalid command!\n\n" << std::flush;
         }
 
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
